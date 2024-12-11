caddd99797eeb469239e57ba57e335deb70ca20b
Beste <fatmaustun1@gmail.com>
2024-12-10 22:45:29 -0600

    Hello Cydeo Class updated

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -1,13 +1,4 @@
 package day01;
 
 public class HelloCydeo {
-    public static void main(String[] args) {
-        System.out.println("Hello Cydeo");
-        Sytem.out.println("Hello World");
-        Sytem.out.println("Hello Programmers");
-        Sytem.out.println("I love coding");
-        
-    
-        
-    }
 }
